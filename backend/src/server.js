67a143b790eefea7c846857f5fed9ff803c02e63
SaiChandraDev <saichandra509001@gmail.com>
2025-10-21 20:06:00 +0530

    Added files to make the application to make production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 
 import notesRoute from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
@@ -12,10 +14,13 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve();
+//can be used in development to resolve cors error between client and server not encountered any errors after deployment to production.
+if (process.env.NODE_ENV !== "production") {
 app.use(cors({
     origin: "http://localhost:5173",
 }))
+}
 //middle-ware, Where this express function helps us in defining the req body in the controller js.
 app.use(express.json());
 
@@ -24,6 +29,14 @@ app.use(rateLimiter);
 
 
 app.use("/api/notes", notesRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 //Connect the DB first then starting the app.(i.e) Application.
 connectDB().then(()=> {
